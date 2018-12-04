d89385c27f6c1dd1e7bb73f3f62416ba8cf4c633
steveblomeley <steveblomeley@yahoo.co.uk>
2018-12-04 12:03:12 +0000

    Display details of selected camera, including 2 way binding for slider control

--- WpfComplexModelBindingPoco/MainPageViewModel.cs
+++ WpfComplexModelBindingPoco/MainPageViewModel.cs
@@ -1,9 +1,31 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using WpfComplexModelBindingPoco.Annotations;
 
 namespace WpfComplexModelBindingPoco
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<Camera> Cameras { get; set; } = new ObservableCollection<Camera>();
+
+        private Camera _selectedCamera;
+        public Camera SelectedCamera
+        {
+            get => _selectedCamera;
+            set
+            {
+                _selectedCamera = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- WpfComplexModelBindingPoco/MainWindow.xaml.cs
+++ WpfComplexModelBindingPoco/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace WpfComplexModelBindingPoco
 {
@@ -12,12 +13,19 @@ namespace WpfComplexModelBindingPoco
 
         public MainWindow()
         {
-            _vm.Cameras.Add(new Camera { Model = "Model AB-c", FirmwareVersion = "9.8.7" });
-            _vm.Cameras.Add(new Camera { Model = "Model x", FirmwareVersion = "1.2.3" });
-            _vm.Cameras.Add(new Camera { Model = "Model Z/1", FirmwareVersion = "4.5.600" });
+            _vm.Cameras.Add(new Camera { Model = "Model AB-c", FirmwareVersion = "9.8.7", Scaling = 7});
+            _vm.Cameras.Add(new Camera { Model = "Model x", FirmwareVersion = "1.2.3", Scaling = 2 });
+            _vm.Cameras.Add(new Camera { Model = "Model Z/1", FirmwareVersion = "4.5.600", Scaling = 10 });
 
             InitializeComponent();
             DataContext = _vm;
+        }
+
+        private void ListOfCameras_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!(e.AddedItems[0] is Camera selectedCamera)) return;
+
+            _vm.SelectedCamera = selectedCamera;
         }
     }
 }
