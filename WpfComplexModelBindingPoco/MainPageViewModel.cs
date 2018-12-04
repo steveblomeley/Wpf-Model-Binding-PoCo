3dc77accd59359d95aeae9c63751d67f7cc1e63a
steveblomeley <steveblomeley@yahoo.co.uk>
2018-12-04 11:25:28 +0000

    Add list of cameras that are a property on the view-model class

---
+++ WpfComplexModelBindingPoco/MainPageViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WpfComplexModelBindingPoco
+{
+    public class MainPageViewModel
+    {
+        public ObservableCollection<Camera> Cameras { get; set; } = new ObservableCollection<Camera>();
+    }
+}
--- WpfComplexModelBindingPoco/MainWindow.xaml.cs
+++ WpfComplexModelBindingPoco/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfComplexModelBindingPoco
 {
@@ -20,9 +8,16 @@ namespace WpfComplexModelBindingPoco
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainPageViewModel _vm = new MainPageViewModel();
+
         public MainWindow()
         {
+            _vm.Cameras.Add(new Camera { Model = "Model AB-c", FirmwareVersion = "9.8.7" });
+            _vm.Cameras.Add(new Camera { Model = "Model x", FirmwareVersion = "1.2.3" });
+            _vm.Cameras.Add(new Camera { Model = "Model Z/1", FirmwareVersion = "4.5.600" });
+
             InitializeComponent();
+            DataContext = _vm;
         }
     }
 }
